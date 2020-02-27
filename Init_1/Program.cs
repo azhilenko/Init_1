d04f8077ad411a8cb8e55c1688a63d599e2e99d3
azhilenko <azhilenko@gmail.com>
2020-02-28 01:42:14 +0200

    IEnumerator

--- Init_1/Program.cs
+++ Init_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,65 @@ using System.Threading.Tasks;
 
 namespace Init_1
 {
+
+    class Week
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        public IEnumerator<string> GetEnumerator() { return new WeekEnumerator(days); }
+
+        class WeekEnumerator : IEnumerator<string>
+        {
+            string[] days;
+            int position = -1;
+            public WeekEnumerator(string[] dayx)
+            {
+                this.days = dayx;
+            }
+            public string Current
+            {
+                get
+                {
+                    if (position == -1 || position >= days.Length)
+                        throw new InvalidOperationException();
+                    return days[position];
+                }
+            }
+            object IEnumerator.Current => throw new NotImplementedException();
+            public bool MoveNext()
+            {
+                if (position < days.Length - 1) { position++; return true; }
+                else
+                    return false;
+            }
+            public void Reset() { position = -1; }
+            public void Dispose() { }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            getfromlist();
-            State.active.GetHashCode();
+            //var x =State.active.GetHashCode();
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Week week = new Week();
+            foreach(var day in week)
+            {
+                Console.WriteLine(day);
+            }
+            
 
 
+
+
+            if (numbers.Length != 0 && numbers != null)
+            foreach (var x in numbers)
+            {
+                Console.WriteLine(x);
+            }
+            else
+                throw new InvalidOperationException();
+            var br =0;
+            Console.ReadLine();
         }
 
         enum State
